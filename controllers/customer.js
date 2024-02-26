1df64565e2cc0ee59101d0b40bd96ef426909e93
chitra0404 <chitrasuresh0404@gmail.com>
2024-02-26 13:45:38 +0530

    commit

--- controllers/customer.js
+++ controllers/customer.js
@@ -39,7 +39,7 @@ const randomString =
 Math.random().toString(36).substring(2, 15) +
 Math.random().toString(36).substring(2, 15);
 
-const link = `https://crm-frontend-c97i.vercel.app//acc/${randomString}`;
+const link = `${process.env.LINK}/acc/${randomString}`;
 
 const sub = "Account Activation"
 
@@ -129,7 +129,7 @@ module.exports.PasswordResetLink = async (req, res) => {
         res.status(200).json({message:"mail sent"});
 
         //sending email for resetting
-        const link = `https://crm-frontend-blush.vercel.app/reset/${randomString}`;
+        const link = `${process.env.LINK}/reset/${randomString}`;
 
         const sub = "Reset password"
 
